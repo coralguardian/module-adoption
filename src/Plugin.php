85f0b56ef6f60f3bdbc3ef83585dc44f09bd4dd7
Grégory COLLIN <gregory@d4rk0s.net>
2022-05-11 13:47:44 +0200

    adjust constant name

--- src/Plugin.php
+++ src/Plugin.php
@@ -4,13 +4,13 @@ namespace D4rk0snet\Adoption;
 
 use D4rk0snet\Adoption\API\IndividualAdoptionEndpoint;
 use D4rk0snet\Adoption\API\NameAdopteesEndpoint;
-use Hyperion\Stripe\Enum\WordpressEventEnum;
+use Hyperion\Stripe\Enum\StripeEventEnum;
 
 class Plugin
 {
     public static function init()
     {
-        add_action(WordpressEventEnum::PAYMENT_SUCCESS->value,'\D4rk0snet\Adoption\Action\PaymentSuccessAction::doAction',10,1);
+        add_action(StripeEventEnum::PAYMENT_SUCCESS->value,'\D4rk0snet\Adoption\Action\PaymentSuccessAction::doAction',10,1);
         do_action(\Hyperion\RestAPI\Plugin::ADD_API_ENDPOINT_ACTION, new IndividualAdoptionEndpoint());
         do_action(\Hyperion\RestAPI\Plugin::ADD_API_ENDPOINT_ACTION, new NameAdopteesEndpoint());
         add_filter(\Hyperion\Doctrine\Plugin::ADD_ENTITIES_FILTER, function(array $entityPaths)
