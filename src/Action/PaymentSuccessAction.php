afbd7da8127e26cdaa504650bcc1cbec3eaaad7f
Grégory COLLIN <gregory@d4rk0s.net>
2022-05-12 22:48:50 +0200

    bugfix and module email dep

--- src/Action/PaymentSuccessAction.php
+++ src/Action/PaymentSuccessAction.php
@@ -5,27 +5,28 @@ namespace D4rk0snet\Adoption\Action;
 use D4rk0snet\Adoption\Entity\AdoptionEntity;
 use D4rk0snet\Email\Event\AdoptionOrder;
 use Hyperion\Doctrine\Service\DoctrineService;
+use Stripe\PaymentIntent;
 
 class PaymentSuccessAction
 {
     /**
      * @todo: Gérer le cas ensuite pour les entreprises
      */
-    public static function doAction($stripePaymentIntent)
+    public static function doAction(PaymentIntent $stripePaymentIntent)
     {
-        if($stripePaymentIntent['metadata']['type'] !== 'adoption') {
+        if($stripePaymentIntent->metadata->type !== 'adoption') {
             return;
         }
 
         // Save Payment reference in order
-        $adoptionUuid = $stripePaymentIntent['metadata']['adoption_uuid'];
+        $adoptionUuid = $stripePaymentIntent->metadata->adoption_uuid;
 
         /** @var AdoptionEntity $entity */
         $entity = DoctrineService::getEntityManager()->getRepository(AdoptionEntity::class)->find($adoptionUuid);
         if($entity === null) {
             return;
         }
-        $entity->setStripePaymentIntentId($stripePaymentIntent['id']);
+        $entity->setStripePaymentIntentId($stripePaymentIntent->id);
         DoctrineService::getEntityManager()->flush();
 
         // Send email event with data needed
