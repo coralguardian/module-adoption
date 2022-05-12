e702d5917dd713e08dbc9105074b3ba904a593d7
Grégory COLLIN <gregory@d4rk0s.net>
2022-05-12 22:18:18 +0200

    Add test values

--- src/Enums/AdoptedProduct.php
+++ src/Enums/AdoptedProduct.php
@@ -12,10 +12,10 @@ enum AdoptedProduct : string
     public function getStripeProductId() : string
     {
         return match($this) {
-            AdoptedProduct::CORAL => 'prod_LS3ht8WpyrWaCA',
-            AdoptedProduct::BUTTERFLY_REEF => 'prod_LS3xkDunBD8suJ',
-            AdoptedProduct::REEF_LADY => 'prod_LS3vPfmveu1RCt',
-            AdoptedProduct::REEF_NAPOLEON => 'prod_LS3ypcdG7tdxBp'
+            AdoptedProduct::CORAL => getenv('STRIPE_MODE') === 'test' ? 'prod_LSVYXFnIStsEzY' : 'prod_LS3ht8WpyrWaCA',
+            AdoptedProduct::BUTTERFLY_REEF => getenv('STRIPE_MODE') === 'test' ? 'prod_LVqtryr8rkKhZ8' : 'prod_LS3xkDunBD8suJ',
+            AdoptedProduct::REEF_LADY => getenv('STRIPE_MODE') === 'test' ? 'prod_LVqvzKfXJGMlrO' : 'prod_LS3vPfmveu1RCt',
+            AdoptedProduct::REEF_NAPOLEON => getenv('STRIPE_MODE') === 'test' ? 'prod_LVqwt7dTeM8Vwl' : 'prod_LS3ypcdG7tdxBp'
         };
     }
 
@@ -32,10 +32,10 @@ enum AdoptedProduct : string
     public function getProductPriceId() : string
     {
         return match($this) {
-            AdoptedProduct::CORAL => 'price_1Kl9ZpLwnTG7uFWwtEBLFM5q',
-            AdoptedProduct::BUTTERFLY_REEF => 'price_1Kl9pkLwnTG7uFWwi2NNcBng',
-            AdoptedProduct::REEF_LADY => 'price_1Kl9niLwnTG7uFWwG3UZnUg0',
-            AdoptedProduct::REEF_NAPOLEON => 'price_1Kl9qELwnTG7uFWwJdi6Mdts'
+            AdoptedProduct::CORAL => getenv('STRIPE_MODE') === 'test' ? 'price_1KlaXDLwnTG7uFWwbEvnynJG' : 'price_1Kl9ZpLwnTG7uFWwtEBLFM5q',
+            AdoptedProduct::BUTTERFLY_REEF => getenv('STRIPE_MODE') === 'test' ? 'price_1KopCBLwnTG7uFWwDaMTjQZv' : 'price_1Kl9pkLwnTG7uFWwi2NNcBng',
+            AdoptedProduct::REEF_LADY => getenv('STRIPE_MODE') === 'test' ? 'price_1KopDSLwnTG7uFWwgf5Gpov4' : 'price_1Kl9niLwnTG7uFWwG3UZnUg0',
+            AdoptedProduct::REEF_NAPOLEON => getenv('STRIPE_MODE') === 'test' ? 'price_1KopEyLwnTG7uFWwvXgb4N5u' : 'price_1Kl9qELwnTG7uFWwJdi6Mdts'
         };
     }
 }
