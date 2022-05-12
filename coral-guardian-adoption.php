98b74a2851d9359cab648b4f72c193a793108df7
Grégory COLLIN <gregory@d4rk0s.net>
2022-05-12 17:33:12 +0200

    Fix add actions

--- coral-guardian-adoption.php
+++ coral-guardian-adoption.php
@@ -10,4 +10,13 @@
  * Licence: GPLv2
  */
 
-add_action('init', 'D4rk0snet\Adoption\Plugin::init');
+use Hyperion\Stripe\Enum\StripeEventEnum;
+
+add_action('plugins_loaded', 'D4rk0snet\Adoption\Plugin::launchActions');
+add_action(StripeEventEnum::PAYMENT_SUCCESS->value,'\D4rk0snet\Adoption\Action\PaymentSuccessAction::doAction',10,1);
+add_filter(\Hyperion\Doctrine\Plugin::ADD_ENTITIES_FILTER, function(array $entityPaths)
+{
+    $entityPaths[] = __DIR__."/Entity";
+
+    return $entityPaths;
+});
--- src/Plugin.php
+++ src/Plugin.php
@@ -4,20 +4,12 @@ namespace D4rk0snet\Adoption;
 
 use D4rk0snet\Adoption\API\IndividualAdoptionEndpoint;
 use D4rk0snet\Adoption\API\NameAdopteesEndpoint;
-use Hyperion\Stripe\Enum\StripeEventEnum;
 
 class Plugin
 {
-    public static function init()
+    public static function launchActions()
     {
-        add_action(StripeEventEnum::PAYMENT_SUCCESS->value,'\D4rk0snet\Adoption\Action\PaymentSuccessAction::doAction',10,1);
         do_action(\Hyperion\RestAPI\Plugin::ADD_API_ENDPOINT_ACTION, new IndividualAdoptionEndpoint());
         do_action(\Hyperion\RestAPI\Plugin::ADD_API_ENDPOINT_ACTION, new NameAdopteesEndpoint());
-        add_filter(\Hyperion\Doctrine\Plugin::ADD_ENTITIES_FILTER, function(array $entityPaths)
-        {
-            $entityPaths[] = __DIR__."/Entity";
-
-            return $entityPaths;
-        });
     }
 }
