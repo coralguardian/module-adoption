e0323857caf3567358a0d5daf73c448065ebc2d8
d4rk0s <gregory@d4rk0s.net>
2022-05-20 16:20:16 +0200

    bugfix

--- src/Action/GiftAdoptionPaymentSuccessAction.php
+++ src/Action/GiftAdoptionPaymentSuccessAction.php
@@ -7,14 +7,12 @@ use D4rk0snet\Adoption\Entity\AdoptionEntity;
 use D4rk0snet\Adoption\Entity\GiftAdoption;
 use D4rk0snet\Email\Event\AdoptionOrder;
 use D4rk0snet\FiscalReceipt\Endpoint\GetFiscalReceiptEndpoint;
+use D4rk0snet\FiscalReceipt\Service\FiscalReceiptService;
 use Hyperion\Doctrine\Service\DoctrineService;
 use Stripe\PaymentIntent;
 
 class GiftAdoptionPaymentSuccessAction
 {
-    /**
-     * @todo: Gérer le cas ensuite pour les entreprises
-     */
     public static function doAction(PaymentIntent $stripePaymentIntent)
     {
         if ($stripePaymentIntent->metadata->type !== GiftAdoptionEndpoint::PAYMENT_INTENT_TYPE) {
@@ -33,12 +31,11 @@ class GiftAdoptionPaymentSuccessAction
         DoctrineService::getEntityManager()->flush();
 
         // Send email event with data needed
-
         AdoptionOrder::send(
             email: $entity->getCustomer()->getEmail(),
             lang: $entity->getLang()->value,
             quantity: $entity->getQuantity(),
-            receiptFileUrl: GetFiscalReceiptEndpoint::getUrl()."?".GetFiscalReceiptEndpoint::ORDER_UUID_PARAM."=".$entity->getUuid(),
+            receiptFileUrl: FiscalReceiptService::getURl($giftAdoptionUuid),
             nextStepUrl: "www.google.fr"
         );
     }
