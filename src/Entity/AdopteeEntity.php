fa6ca0700027f8341b95d964293d097c5648dc6e
d4rk0s <gregory@d4rk0s.net>
2022-07-07 18:37:40 +0200

    Passage en uuid standard

--- src/Entity/AdopteeEntity.php
+++ src/Entity/AdopteeEntity.php
@@ -19,7 +19,7 @@ class AdopteeEntity
 {
     /**
      * @ORM\Id
-     * @ORM\Column(type="uuid_binary_ordered_time", unique=true)
+     * @ORM\Column(type="uuid", unique=true)
      * @ORM\GeneratedValue(strategy="CUSTOM")
      * @ORM\CustomIdGenerator(class="Ramsey\Uuid\Doctrine\UuidGenerator")
      */
--- src/Entity/Friend.php
+++ src/Entity/Friend.php
@@ -13,7 +13,7 @@ class Friend
 {
     /**
      * @ORM\Id
-     * @ORM\Column(type="uuid_binary_ordered_time", unique=true)
+     * @ORM\Column(type="uuid", unique=true)
      * @ORM\GeneratedValue(strategy="CUSTOM")
      * @ORM\CustomIdGenerator(class="Ramsey\Uuid\Doctrine\UuidGenerator")
      */
