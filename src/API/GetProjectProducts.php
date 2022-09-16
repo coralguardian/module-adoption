de50066f44587ef05cd0f690e3d14600be63665c
Benoit Delboé <benoit@lescanumeriques.fr>
2022-09-16 12:59:01 +0200

    sort projectProducts by price asc

--- src/API/GetProjectProducts.php
+++ src/API/GetProjectProducts.php
@@ -26,7 +26,7 @@ class GetProjectProducts extends APIEnpointAbstract
         $productModels = [];
 
         /** @var Product $product */
-        foreach($stripeProducts->data as $product) {
+        foreach ($stripeProducts->data as $product) {
             $price = SearchService::getPrice($product->default_price);
 
             $productModels[] = (new ProjectProducts())
@@ -35,10 +35,12 @@ class GetProjectProducts extends APIEnpointAbstract
                 ->setVariant($product->metadata['variant']);
         }
 
-        if(count($stripeProducts) === 0) {
+        if (count($stripeProducts) === 0) {
             return APIManagement::APINotFound("");
         }
-        usort($productModels, function(ProjectProducts $a, ProjectProducts $b) {return $a->getPrice() - $b->getPrice();});
+        usort($productModels, function (ProjectProducts $a, ProjectProducts $b) {
+            return $a->getPrice() - $b->getPrice();
+        });
         return APIManagement::APIOk($productModels);
     }
 
