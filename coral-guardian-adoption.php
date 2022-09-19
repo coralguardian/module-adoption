05dee6d6fafcf55f24586d0b77ea4063f714159a
d4rk0s <gregory@d4rk0s.net>
2022-09-19 14:34:31 +0200

    bugfix

--- coral-guardian-adoption.php
+++ coral-guardian-adoption.php
@@ -23,7 +23,7 @@ add_action('plugins_loaded', [\D4rk0snet\Adoption\Plugin::class,'launchActions']
 add_action(CoralOrderEvents::NEW_ORDER->value, [NewOrderListener::class, 'doAction'], 10,1);
 add_action(CoralAdoptionActions::PENDING_ADOPTION->value, [CreateAdoption::class, 'doAction'], 10,1);
 add_action(CoralAdoptionActions::PENDING_GIFT_ADOPTION->value, [CreateGiftAdoption::class, 'doAction'], 10,1);
-add_filter(CoralAdoptionFilters::GET_GIFTADOPTION->value, [GetGiftAdoptionFilter::class, 'doAction']);
+add_filter(CoralAdoptionFilters::GET_GIFTADOPTION->value, [GetGiftAdoptionFilter::class, 'doAction'], 10, 2);
 add_filter(Plugin::ADD_ENTITIES_FILTER, function (array $entityPaths) {
     $entityPaths[] = __DIR__."/src/Entity";
 
