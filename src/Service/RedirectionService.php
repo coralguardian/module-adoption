cc12550ca17a09f62c119e479ed673e419944884
Gregory Collin <gregory@d4rk0s.net>
2022-12-13 12:27:11 +0100

    Demande 108 changement url

--- src/Service/RedirectionService.php
+++ src/Service/RedirectionService.php
@@ -4,13 +4,14 @@ namespace D4rk0snet\Adoption\Service;
 
 use D4rk0snet\Adoption\Entity\AdoptionEntity;
 use D4rk0snet\Adoption\Enums\RedirectionStep;
+use D4rk0snet\CoralCustomer\Entity\CompanyCustomerEntity;
 use WP_Query;
 
 class RedirectionService
 {
     public static function buildRedirectionUrl(AdoptionEntity $adoptionEntity): string
     {
-        $baseUrl = home_url("adoption-entreprise");
+        $baseUrl = $adoptionEntity->getCustomer() instanceof CompanyCustomerEntity ? home_url("adoption-entreprise") : home_url("adopte-corail");
         $baseUrl .=  "?adoptionUuid=" . $adoptionEntity->getUuid() .
             "&step=" . RedirectionStep::getEnumBasedOnClass($adoptionEntity::class)->value;
 
