50f3da2cb9d890f7ff93f4d91c1e73a241c9f5cc
Gregory Collin <gregory@d4rk0s.net>
2022-12-15 11:09:20 +0100

    Demande 111 - Add language in url for next step email

--- src/Service/RedirectionService.php
+++ src/Service/RedirectionService.php
@@ -5,6 +5,7 @@ namespace D4rk0snet\Adoption\Service;
 use D4rk0snet\Adoption\Entity\AdoptionEntity;
 use D4rk0snet\Adoption\Enums\RedirectionStep;
 use D4rk0snet\CoralCustomer\Entity\CompanyCustomerEntity;
+use D4rk0snet\Coralguardian\Enums\Language;
 use WP_Query;
 
 class RedirectionService
@@ -12,6 +13,7 @@ class RedirectionService
     public static function buildRedirectionUrl(AdoptionEntity $adoptionEntity): string
     {
         $baseUrl = $adoptionEntity->getCustomer() instanceof CompanyCustomerEntity ? home_url("adoption-entreprise") : home_url("adopte-corail");
+        $baseUrl .= $adoptionEntity->getLang() === Language::EN ? '/en/': "";
         $baseUrl .=  "?adoptionUuid=" . $adoptionEntity->getUuid() .
             "&step=" . RedirectionStep::getEnumBasedOnClass($adoptionEntity::class)->value;
 
