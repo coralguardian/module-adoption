0b4bfe6aa410ea99e44a473d3964a7b060ebbd26
Gregory Collin <gregory@d4rk0s.net>
2022-12-19 10:44:45 +0100

    Bugfix ticket 111 : New url according to language

--- src/Service/RedirectionService.php
+++ src/Service/RedirectionService.php
@@ -12,8 +12,11 @@ class RedirectionService
 {
     public static function buildRedirectionUrl(AdoptionEntity $adoptionEntity): string
     {
-        $baseUrl = $adoptionEntity->getCustomer() instanceof CompanyCustomerEntity ? home_url("adoption-entreprise") : home_url("adopte-corail");
-        $baseUrl .= $adoptionEntity->getLang() === Language::EN ? '/en/': "";
+        if($adoptionEntity->getLang() === Language::EN) {
+            $baseUrl = $adoptionEntity->getCustomer() instanceof CompanyCustomerEntity ? home_url("csr-coral-guardian"): home_url("adopt-a-coral");
+        } else {
+            $baseUrl = $adoptionEntity->getCustomer() instanceof CompanyCustomerEntity ? home_url("adoption-entreprise") : home_url("adopte-corail");
+        }
         $baseUrl .=  "?adoptionUuid=" . $adoptionEntity->getUuid() .
             "&step=" . RedirectionStep::getEnumBasedOnClass($adoptionEntity::class)->value;
 
